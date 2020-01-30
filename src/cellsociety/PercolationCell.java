8f8a4396417e995e4c9b7fbdf38259dc98919bf2
Thomas Quintanilla <tq11@duke.edu>
2020-01-30 02:06:42 -0500

    Created Grid Class with appropriate methods, abstract Cell class with appropriate methods, and skeletons of FireCell, GameOfLifeCell, PercolationCell, PredatorPreyCell, and SegregationCell

---
+++ src/cellsociety/PercolationCell.java
@@ -0,0 +1,8 @@
+package cellsociety;
+
+public class PercolationCell extends Cell {
+    @Override
+    public int calculateNextState() {
+        return 0;
+    }
+}
---
+++ src/cellsociety/SegregationCell.java
@@ -0,0 +1,8 @@
+package cellsociety;
+
+public class SegregationCell extends Cell {
+    @Override
+    public int calculateNextState() {
+        return 0;
+    }
+}
