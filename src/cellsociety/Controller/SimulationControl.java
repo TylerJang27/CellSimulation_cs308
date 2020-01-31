5564b13b13e65da16804935cd17a09b3c005bb86
Mariusz Derezinski-Choo <mnd14@duke.edu>
2020-01-30 20:49:35 -0500

    added event listeners in SimulationControl Branch

--- src/cellsociety/Controller/SimulationControl.java
+++ src/cellsociety/Controller/SimulationControl.java
@@ -1,8 +1,15 @@
 package cellsociety.Controller;
 
+import cellsociety.View.ApplicationView;
+import javafx.application.Application;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.event.EventHandler;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
 import java.io.File;
+import java.sql.SQLOutput;
 
 /**
  * Core class of the Controller part of the MVC Model
@@ -17,7 +24,15 @@ public class SimulationControl {
     //private Grid myGrid;
     //private ApplicationView myView;
     private Simulation mySim;
+    private ApplicationView myApplicationView;
 
+    public SimulationControl(Stage primaryStage){
+        EventHandler<MouseEvent> playButtonClickedHandler = getPlayHandler();
+        EventHandler<MouseEvent> pauseButtonClickedHandler = getPauseListener();
+        EventHandler<MouseEvent> stepButtonClickedHandler = getStepHandler();
+        ChangeListener<? super Number> sliderListener = getSliderListener();
+        myApplicationView = new ApplicationView(800, primaryStage,playButtonClickedHandler,pauseButtonClickedHandler,stepButtonClickedHandler,sliderListener);
+    }
 
     public SimulationControl(String fname, Stage primaryStage) {
         //
@@ -25,6 +40,21 @@ public class SimulationControl {
         //
     }
 
+    private void pauseSimulation() {
+        System.out.println("Pause the Simulation");
+    }
+
+    private void stepSimulation() {
+        System.out.println("step the simulation");
+    }
+
+    private void changeSimulationSpeed(Number value) {
+        System.out.println(value);
+    }
+
+    private void playSimulation() {
+        System.out.println("We are all living in a simulation, and now it is playing");
+    }
     public void next() {
         //update grid
 
@@ -47,5 +77,46 @@ public class SimulationControl {
     public void changeModelSetup(String fname) {
 
     }
+
+    private EventHandler<MouseEvent> getPlayHandler() {
+        EventHandler<MouseEvent> playButtonClickedHandler = new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                playSimulation();
+            }
+        };
+        return playButtonClickedHandler;
+    }
+
+    private EventHandler<MouseEvent> getPauseListener() {
+        EventHandler<MouseEvent> pauseButtonClickedHandler = new EventHandler<>() {
+            @Override
+            public void handle(MouseEvent event) {
+                pauseSimulation();
+            }
+        };
+        return pauseButtonClickedHandler;
+    }
+
+    private EventHandler<MouseEvent> getStepHandler() {
+        EventHandler<MouseEvent> stepButtonClickedHandler = new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                stepSimulation();
+            }
+        };
+        return stepButtonClickedHandler;
+    }
+
+    private ChangeListener<? super Number> getSliderListener() {
+        ChangeListener<? super Number> sliderListener = new ChangeListener<Number>() {
+            @Override
+            public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
+                changeSimulationSpeed(observable.getValue());
+            }
+        };
+        return sliderListener;
+    }
+
 
 }
