9bb92d165d03680b0e0479ac1ffcf1837b3cd659
Mariusz Derezinski-Choo <mnd14@duke.edu>
2020-02-10 00:11:47 -0500

    refactored CellState

--- src/cellsociety/View/CellState.java
+++ src/cellsociety/View/CellState.java
@@ -1,13 +1,28 @@
 package cellsociety.View;
 
 import javafx.scene.Node;
-import javafx.scene.layout.Pane;
 
+/**
+ * An abstract class representing the State of a single cell. Each cell can change between several CellStates according to the simulation
+ * Each CellState has an ID which specifies what state it represents ("predator" vs "prey", etc). this id is displayed in the
+ * line graph of the different cells
+ *
+ * @author Mariusz Derezinski-Choo
+ */
 public abstract class CellState{
     protected String myID;
 
+    /**
+     * get the description of the CellState, used to differentiate between different states in a human-readable manner
+     * @return the id of the CellState
+     */
     public String getStateDescription(){
         return myID;
     }
+
+    /**
+     * return the Node representing this state so that it can be rendered on the Grid
+     * @return the Node representing the appearance of this state
+     */
     public abstract Node getNode();
 }
