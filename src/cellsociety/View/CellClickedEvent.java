eefc53314e6c888ce7aa571ba11a21a1bb3b3a0a
Mariusz Derezinski-Choo <mnd14@duke.edu>
2020-02-10 00:08:13 -0500

    refatored CellClickedEvent

--- src/cellsociety/View/CellClickedEvent.java
+++ src/cellsociety/View/CellClickedEvent.java
@@ -3,8 +3,13 @@ package cellsociety.View;
 import javafx.event.Event;
 import javafx.event.EventType;
 
-import java.awt.event.MouseEvent;
-
+/**
+ * A class that wraps a MouseEvent, but with additional instance variables that specify which Cell the Mouse clicked. This allows
+ * for all Cells to be attached to the same EventHandler, which will delegate tasks accordingly with respect to the row and
+ * column of the cell clicked
+ *
+ * @author Mariusz Derezinski-Choo
+ */
 public class CellClickedEvent extends Event {
     public static final EventType<CellClickedEvent> CUSTOM_EVENT_TYPE = new EventType(ANY);
 
@@ -12,9 +17,12 @@ public class CellClickedEvent extends Event {
     private int myRow;
     private int myColumn;
 
-    public CellClickedEvent(EventType<? extends Event> eventType) {
-        super(eventType);
-    }
+    /**
+     * Construct a CellClickedEvent
+     * @param cell the CellView that was clicked
+     * @param row the row of the cell that was clicked with respect to the grid it is in
+     * @param column the column of the cell that was clicked with respect to the grid it is in
+     */
     public CellClickedEvent(CellView cell, int row, int column){
         super(CUSTOM_EVENT_TYPE);
         myRow = row;
@@ -22,14 +30,26 @@ public class CellClickedEvent extends Event {
         myCell = cell;
     }
 
+    /**
+     * getter method for the Cell that was clicked
+     * @return the cell that was clicked
+     */
     public CellView getCell(){
         return myCell;
     }
 
+    /**
+     * getter method for the row of the cell
+     * @return the row of the cell
+     */
     public int getRow(){
         return myRow;
     }
 
+    /**
+     * getter method for the column of the cell
+     * @return the column of the cell
+     */
     public int getColumn(){
         return myColumn;
     }
